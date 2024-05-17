fa7315a5fd90cf9d788e43664c4b881876e1fa3f
dona <ehsk0123@naver.com>
2024-05-18 01:28:45 +0900

    [240518] 제품 추가 기능 구현

--- src/AddShop.js
+++ src/AddShop.js
@@ -1,86 +1,80 @@
 import React, { useState } from "react";
-import { Button, Grid, TextField } from "@mui/material";
+import { Button, Grid, TextField, Typography } from "@mui/material";
 
 const AddShop = (props) => {
-  // 사용자의 입력을 저장할 오브젝트
-  const [item, setItem] = useState({ title: "" });
-  const addItem = props.addItem; // 부모가 전달해준 함수
+  const [item, setItem] = useState({ title: "", userId: "", ingredient: "", crisp: "" });
+  const addItem = props.addItem;
 
-  // 입력 값이 변경될 때 호출되는 함수
   const onInputChange = (e) => {
-    setItem({ title: e.target.value });
-    console.log(item);
+    const { name, value } = e.target;
+    setItem({ ...item, [name]: value });
   };
 
-  // 추가 버튼 클릭 시 호출되는 함수
-  const onButtonClick = (e) => {
+  const onButtonClick = () => {
     addItem(item);
-    setItem({ title: "" }); // 입력 값 초기화하여 리렌더링 발생
+    setItem({ title: "", userId: "", ingredient: "", crisp: "" });
   };
-
-  const enterEventHandler = (e) => {
-    if (e.key === 'Enter') {
-      onButtonClick(); 
-    }
-  }
 
   return (
-    <Grid container spacing={2} alignItems="center" style={{ marginTop: 20 }}>
-      <Grid item xs={12}>
-        <span style={{ marginRight: 8 }}>title:</span>
-        <TextField
-          name="title"
-          placeholder="Title"
-          fullWidth
-          onChange={onInputChange}
-          onKeyPress={enterEventHandler}
-          value={item.title}
-        />
+    <div>
+      <Grid container spacing={2} alignItems="center" style={{ marginTop: 20 }}>
+        <Grid item xs={12}>
+          <Typography variant="h5" align="center" gutterBottom>
+          ― 제품 추가 ―
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <span style={{ marginRight: 8 }}>Title:</span>
+          <TextField
+            name="title"
+            placeholder="제품명을 입력하세요"
+            fullWidth
+            onChange={onInputChange}
+            value={item.title}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <span style={{ marginRight: 8 }}>User ID:</span>
+          <TextField
+            name="userId"
+            placeholder="사용자 ID를 입력하세요"
+            fullWidth
+            onChange={onInputChange}
+            value={item.userId}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <span style={{ marginRight: 8 }}>Ingredient:</span>
+          <TextField
+            name="ingredient"
+            placeholder="성분을 입력하세요"
+            fullWidth
+            onChange={onInputChange}
+            value={item.ingredient}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <span style={{ marginRight: 8 }}>Crisp:</span>
+          <TextField
+            name="crisp"
+            placeholder="바삭함 여부를 입력하세요"
+            fullWidth
+            onChange={onInputChange}
+            value={item.crisp}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            fullWidth
+            color="secondary"
+            variant="outlined"
+            onClick={onButtonClick}
+          >
+            제품 추가
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <span style={{ marginRight: 8 }}>userId:</span>
-        <TextField
-          name="userId"
-          placeholder="User ID"
-          fullWidth
-          onChange={onInputChange}
-          onKeyPress={enterEventHandler}
-          value={item.userId}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <span style={{ marginRight: 8 }}>ingredient:</span>
-        <TextField
-          name="ingredient"
-          placeholder="Ingredient"
-          fullWidth
-          onChange={onInputChange}
-          onKeyPress={enterEventHandler}
-          value={item.ingredient}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <span style={{ marginRight: 8 }}>crisp:</span>
-        <TextField
-          name="crisp"
-          placeholder="Crisp"
-          fullWidth
-          onChange={onInputChange}
-          onKeyPress={enterEventHandler}
-          value={item.crisp}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <Button
-          fullWidth
-          color="secondary"
-          variant="outlined"
-          onClick={() => onButtonClick('add')}
-        >
-          제품 검색
-        </Button>
-      </Grid>
-    </Grid>
+    </div>
   );
 };
 
